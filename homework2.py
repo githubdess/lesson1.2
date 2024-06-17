8d9b38976dd7e02b78396e665d85e340971187f6
githubdess <dess_81@mail.ru>
2024-06-17 14:18:21 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,11 @@
+# Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
+
+b = 'всего задач:'
+bb = the_number_of_completed_DZ = 12
+c = 'затрачено часов:'
+cc = number_of_hours_spent = 1.5
+a = 'Курс:'
+aa = course_name = 'Python'
+d = time_for_one_task = 'среднее время выполнения:'
+e = number_of_hours_spent / the_number_of_completed_DZ
+print(a, aa, ',', b, bb, ',', c, cc, ',', d, e, 'часа', '.')
